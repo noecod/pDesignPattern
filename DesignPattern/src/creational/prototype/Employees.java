5708324e6f51b3c7173c7810778727201593c9ae
rene.notter <rene.notter@iaeth.ch>
2019-05-30 09:08:13 +0200

    prototype

---
+++ DesignPattern/src/creational/prototype/Employees.java
@@ -0,0 +1,40 @@
+package creational.prototype;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.Getter;
+
+@Getter
+public class Employees implements Cloneable {
+
+    private List<String> empList;
+
+    public Employees() {
+        empList = new ArrayList<String>();
+    }
+
+    public Employees(List<String> list) {
+        this.empList = list;
+    }
+
+    public void loadData() {
+        // read all employees from database and put into the list
+        empList.add("Pankaj");
+        empList.add("Raj");
+        empList.add("David");
+        empList.add("Lisa");
+    }
+
+    /**
+     * Deep copy, see code about clone in the Java project.
+     */
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        List<String> temp = new ArrayList<String>();
+        for (String s : this.getEmpList()) {
+            temp.add(s);
+        }
+        return new Employees(temp);
+    }
+}
