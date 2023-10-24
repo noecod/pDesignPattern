e8d046d7465e3874ad4775fbfd80a606373614f9
rene.notter <rene.notter@zoho.com>
2023-10-24 22:38:36 +0200

    style(facade): clean up code

--- DesignPattern/src/structural/facade/report/Report.java
+++ DesignPattern/src/structural/facade/report/Report.java
@@ -4,20 +4,21 @@ import lombok.Getter;
 
 @Getter
 public class Report {
+
     private ReportHeader header;
     private ReportData data;
     private ReportFooter footer;
-     
+
     public void setHeader(ReportHeader header) {
         System.out.println("Setting report header");
         this.header = header;
     }
-    
+
     public void setData(ReportData data) {
         System.out.println("Setting report data");
         this.data = data;
     }
-    
+
     public void setFooter(ReportFooter footer) {
         System.out.println("Setting report footer");
         this.footer = footer;
